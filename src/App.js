f85d43b0273e22f31f1b3498796973b4e1e2b2d5
Swati Avhad <swatiavhad91@gmail.com>
2021-07-26 16:49:40 +0530

    Updated w9ass4

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h3 style={{textAlign:"center"}}>* * * * User Cards Using Redux Saga * * * *</h3><br></br>
       <Users />
     </div>
   );
